c60f872d693bb8cbdd3f6eafe907db210961e82b
CYUSA Asaph <117106119+asaphcyusa01@users.noreply.github.com>
2023-09-14 14:42:05 +0300

    Update account-dashboard.js

--- app/account-dashboard.js
+++ app/account-dashboard.js
@@ -4,52 +4,59 @@ import {
   PhantomWalletAdapter,
 } from '@solana/wallet-adapter-bitpie'; // Replace with your Solana wallet adapters
 import './dashboard.css';
+
 const AccountDashboard = () => {
   const [connectedWallet, setConnectedWallet] = useState(null);
   const [nfts, setNFTs] = useState([]);
   const [transactionHistory, setTransactionHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const connectBitpieWallet = async () => {
+  const connectWallet = async (walletAdapter) => {
     try {
-      const wallet = new BitpieWalletAdapter();
+      const wallet = new walletAdapter();
       await wallet.connect();
       setConnectedWallet(wallet);
     } catch (error) {
-      console.error('Error connecting Bitpie wallet:', error);
-    }
-  };
-
-  const connectPhantomWallet = async () => {
-    try {
-      const wallet = new PhantomWalletAdapter();
-      await wallet.connect();
-      setConnectedWallet(wallet);
-    } catch (error) {
-      console.error('Error connecting Phantom wallet:', error);
+      setError('Error connecting wallet: ' + error.message);
     }
   };
 
   const fetchNFTs = async () => {
-    if (connectedWallet) {
-      // Simulate fetching NFTs with placeholder data
-      const sampleNFTs = [
-        { name: 'NFT 1', id: 1 },
-        { name: 'NFT 2', id: 2 },
-        { name: 'NFT 3', id: 3 },
-      ];
-      setNFTs(sampleNFTs);
+    setLoading(true);
+    try {
+      if (connectedWallet) {
+        // Replace with real data fetching
+        const sampleNFTs = [
+          { name: 'NFT 1', id: 1 },
+          { name: 'NFT 2', id: 2 },
+          { name: 'NFT 3', id: 3 },
+        ];
+        setNFTs(sampleNFTs);
+      }
+    } catch (error) {
+      setError('Error fetching NFTs: ' + error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   const fetchTransactionHistory = async () => {
-    if (connectedWallet) {
-      // Simulate fetching transaction history with placeholder data
-      const sampleTransactions = [
-        { description: 'Transaction 1', id: 1 },
-        { description: 'Transaction 2', id: 2 },
-        { description: 'Transaction 3', id: 3 },
-      ];
-      setTransactionHistory(sampleTransactions);
+    setLoading(true);
+    try {
+      if (connectedWallet) {
+        // Replace with real data fetching
+        const sampleTransactions = [
+          { description: 'Transaction 1', id: 1 },
+          { description: 'Transaction 2', id: 2 },
+          { description: 'Transaction 3', id: 3 },
+        ];
+        setTransactionHistory(sampleTransactions);
+      }
+    } catch (error) {
+      setError('Error fetching transaction history: ' + error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -62,11 +69,18 @@ const AccountDashboard = () => {
 
   return (
     <div>
-      {connectedWallet ? (
+      {error && <div className="error">{error}</div>}
+      {loading ? (
+        <div>Loading...</div>
+      ) : connectedWallet ? (
         <div>
           <h2>Welcome, {connectedWallet.name}!</h2>
-          <button onClick={connectBitpieWallet}>Connect Bitpie Wallet</button>
-          <button onClick={connectPhantomWallet}>Connect Phantom Wallet</button>
+          <button onClick={() => connectWallet(BitpieWalletAdapter)}>
+            Connect Bitpie Wallet
+          </button>
+          <button onClick={() => connectWallet(PhantomWalletAdapter)}>
+            Connect Phantom Wallet
+          </button>
           <h3>Your NFTs:</h3>
           <ul>
             {nfts.map((nft) => (
@@ -83,8 +97,12 @@ const AccountDashboard = () => {
       ) : (
         <div>
           <h2>Connect your wallet</h2>
-          <button onClick={connectBitpieWallet}>Connect Bitpie Wallet</button>
-          <button onClick={connectPhantomWallet}>Connect Phantom Wallet</button>
+          <button onClick={() => connectWallet(BitpieWalletAdapter)}>
+            Connect Bitpie Wallet
+          </button>
+          <button onClick={() => connectWallet(PhantomWalletAdapter)}>
+            Connect Phantom Wallet
+          </button>
         </div>
       )}
     </div>
